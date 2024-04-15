8de375e18ab7a679f0774febc4e71297aebe060d
Shivanand <datasnekatpure@gmail.com>
2024-04-15 10:42:02 +0530

    updated greynoise

--- greynoise.py
+++ greynoise.py
@@ -13,16 +13,14 @@ time.sleep(1)
 # Locate the text input element using XPath
 text_element = driver.find_element(By.XPATH, "//div[@class='gn-menu inline-block w-full hidden md:inline-block']//div[@id='textInput']")
 text_element.click()
-time.sleep(3)
 
 # Enter the IP address (e.g., "49.36.51.230")
-ip_address = "49.36.51.24"
+ip_address = "192.168.29.222"
 text_element.send_keys(ip_address)
 
-# Get new URL after clicking 'Check IP Address' button
-new_url = driver.current_url
-print(f"new URL after clicking 'Check IP Address' button: {new_url}")
+driver.find_element(By.XPATH, "//div[@class='gn-menu inline-block w-full hidden md:inline-block']//div[@placeholder='Search for IP Addresses, CVEs, Tags...']//button[@type='button']").click()
+print("Clicked on search button")
+
+
 
 time.sleep(15)
-# You can perform further actions or close the driver as needed
-# driver.close()
